6c111e42744506a172f98421a3ed9df12d597f39
CheaVannak007 <vannakchea7@gmail.com>
2017-06-22 20:13:22 +0700

    Project

---
+++ src/main/java/org/khmeracademy/app/TestTemplateAdminThymeleafApplication.java
@@ -0,0 +1,12 @@
+package org.khmeracademy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTemplateAdminThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTemplateAdminThymeleafApplication.class, args);
+	}
+}
